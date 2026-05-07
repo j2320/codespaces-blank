4ad4e074e1ae7afe57de18815a4d34c3f00ebe88
j2320 <j2320@matsue-ct.ac.jp>
2026-05-07 07:20:06 +0000

    READMEとフィズバズ追加

---
+++ Fizz Buzz.js
@@ -0,0 +1,12 @@
+/**
+ * Implement the solution in this function
+ *
+ * @param {number} n Output goes from 1 to n
+ * @returns {(number|string)[]} An array containing all numbers and strings
+ */
+function fizzbuzz (n) {
+    return [n];
+}
+
+let result=fizzbuzz(20);
+console.log(result);
